7fae4e748cf14e0ffe88875e6e6d4e24085d2a98
lalouikarim <lalouikarim@gmail.com>
2022-02-23 14:48:19 +0100

    added an input controller

---
+++ Controllers/InputController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Input{
+    public $value;
+
+    public function __construct($value){
+        $this->value = $value;
+    }
+
+    public function Sanitize(){
+        $this->value= trim($this->value);
+        $this->value= stripslashes($this->value);
+        $this->value = htmlspecialchars($this->value);
+        return $this->value;
+    }
+}
+
+?>
