0399acb271c6327c55aaedc2ecc0a2dc0335515e
lalouikarim <lalouikarim@gmail.com>
2022-02-26 17:41:16 +0100

    added a parent controller class for all cv sections

---
+++ Controllers/CvSectionController.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class CvSectionController{
+    protected $sectionModel;
+
+    public function __construct($sectionModel){
+        // create a section model
+        require "../Models/" . $sectionModel . ".php";
+        $this->sectionModel = new $sectionModel;
+    }
+
+    public function performAction($action){
+        switch($action){
+            case "save":
+                $this->SaveData();
+                break;
+            case "delete":
+                // TODO
+                break;
+            default:
+                break;
+        }
+    }
+
+    abstract public function SaveData();
+    //abstract public function DeleteData($ColumnsValues);
+}
+
+?>
