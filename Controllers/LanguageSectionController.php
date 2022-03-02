9638d7ec81c228a4704cfc4edef878196ff98e56
lalouikarim <lalouikarim@gmail.com>
2022-03-02 17:19:23 +0100

    users can delete a language

--- Controllers/LanguageSectionController.php
+++ Controllers/LanguageSectionController.php
@@ -35,6 +35,7 @@ class LanguageSectionController extends CvSectionController{
                         $oldLanguageLevel->value = null;
                     }
 
+                    // ensure a valid level format
                     if(preg_match("/^(0|[1-9][0-9]|100)$/", $newLanguageLevel->value) && (preg_match("/^(0|[1-9][0-9]|100)$/", $oldLanguageLevel->value) || $oldLanguageLevel->value == null)){
                         // get the user id
                         $userId = $this->sectionModel->auth->getUserId();
@@ -60,7 +61,44 @@ class LanguageSectionController extends CvSectionController{
 
     // delete a language
     public function DeleteData(){
+        // this array will be sent as a response to the client
+        $response_array["action_completed"] = false;
+        $response_array["error"] = "";
         
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["old_language_name"]) && isset($_POST["old_language_level"])){
+            try{
+                if($this->sectionModel->auth->isLoggedIn()){
+                    // indicate that the user is logged in
+                    $response_array["logged_in"] = true;
+
+                    // sanitize the user's input
+                    $oldLanguageName = new Input($_POST["old_language_name"]);
+                    $oldLanguageName->Sanitize();
+                    $oldLanguageLevel = new Input($_POST["old_language_level"]);
+                    $oldLanguageLevel->Sanitize();
+
+                    // ensure a valid level format
+                    if(preg_match("/^(0|[1-9][0-9]|100|)$/", $oldLanguageLevel->value)){
+                        // get the user id
+                        $userId = $this->sectionModel->auth->getUserId();
+
+                        // delete the language
+                        $this->sectionModel->DeleteData(array("user_id" => $userId, "old_language_name" => $oldLanguageName->value, "old_language_level" => $oldLanguageLevel->value));
+
+                        // indicate that the action has completed
+                        $response_array["action_completed"] = true;
+                    } else{
+                        $response_array["error"] = "Invalid level";
+                    }
+                } else{
+                    $response_array["logged_in"] = false;
+                }
+            } catch (Exception $e) {
+                $response_array["error"] = $e->getMessage();
+            }
+        }
+        
+        echo json_encode($response_array);
     }
 
     // add another language subsection
