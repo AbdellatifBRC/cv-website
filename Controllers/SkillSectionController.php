7e2608a7a7135b5a5af7f31f5d4b128516370ecc
lalouikarim <lalouikarim@gmail.com>
2022-03-03 10:59:55 +0100

    users can add another skill subsection

--- Controllers/SkillSectionController.php
+++ Controllers/SkillSectionController.php
@@ -103,7 +103,40 @@ class SkillSectionController extends CvSectionController{
 
     // add another skill subsection
     public function AddSubsecToSec(){
+        // this array will be sent as a response to the client
+        $response_array["action_completed"] = false;
+        $response_array["error"] = "";
+        $response_array["new_subsec_html"] = "";
         
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["subsecs_in_section"])){
+            try{
+                // sanitize the user's input
+                $skillsNumber = new Input($_POST["subsecs_in_section"]);
+                $skillsNumber->Sanitize();
+                if(preg_match("/^(0|[1-9]+[0-9]*)$/", $skillsNumber->value)){
+                    $response_array["new_subsec_html"] = "
+                    <div class='skill' id='skill_" . strval($skillsNumber->value + 1) . "'>
+                        <form id='save_skill_" . strval($skillsNumber->value + 1) . "_section_form'>
+                            <input type='text' name='skill_name' placeholder='Skill'>
+                            <input type='text' name='skill_level' placeholder='Skill Level'>
+                            <button type='submit' onclick=" . '"'. "ModifySection('skill_" . strval($skillsNumber->value + 1) . "', 'save', 'SkillSectionController')" . '"'. ">Save Skill</button>
+                        </form>
+                        <form id='delete_skill_" . strval($skillsNumber->value + 1) . "_section_form'>
+                            <button type='submit' onclick=" . '"' . "ModifySection('skill_" . strval($skillsNumber->value + 1) . "', 'delete', 'SkillSectionController')" . '"' . ">Delete Skill</button>
+                        </form>
+                    </div>";
+
+                    // indicate that the action has completed
+                    $response_array["action_completed"] = true;
+                } else{
+                    $response_array["error"] = "Invalid data";
+                }
+            } catch (Exception $e) {
+                $response_array["error"] = $e->getMessage();
+            }
+        }
+
+        echo json_encode($response_array);
     }
 }
 
