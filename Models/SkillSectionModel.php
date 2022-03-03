4d94bb603f5bfd95b71cbf93618ac155ee8d4971
lalouikarim <lalouikarim@gmail.com>
2022-03-03 10:48:16 +0100

    added a model for the skills section

---
+++ Models/SkillSectionModel.php
@@ -0,0 +1,48 @@
+<?php
+
+// require the parent class
+require "CvSectionModel.php";
+
+class SkillSectionModel extends CvSectionModel{
+    // insert a skill
+    public function InsertData($ColumnsValues){
+        // check if the user already saved this skill
+        $stmt = $this->dbconn->prepare("SELECT id FROM skills_section WHERE user_id = :user_id AND skill_name = :old_skill_name AND skill_level = :old_skill_level");
+        $stmt->bindParam(":user_id", $ColumnsValues["user_id"]);
+        $stmt->bindParam(":old_skill_name", $ColumnsValues["old_skill_name"]);
+        $stmt->bindParam(":old_skill_level", $ColumnsValues["old_skill_level"]);
+        $stmt->execute();
+        // if the user already saved this skill then update it
+        if(count($stmt->fetchAll()) === 1){
+            $this->UpdateData($ColumnsValues);
+        } else{
+            $stmt = $this->dbconn->prepare("INSERT INTO skills_section (user_id, skill_name, skill_level) VALUES (:user_id, :new_skill_name, :new_skill_level)");
+            $stmt->bindParam(":user_id", $ColumnsValues["user_id"]);
+            $stmt->bindParam(":new_skill_name", $ColumnsValues["new_skill_name"]);
+            $stmt->bindParam(":new_skill_level", $ColumnsValues["new_skill_level"]);
+            $stmt->execute();
+        }
+    }
+
+    // update a skill
+    public function UpdateData($ColumnsValues){
+        $stmt = $this->dbconn->prepare("UPDATE skills_section SET skill_name = :new_skill_name, skill_level = :new_skill_level WHERE user_id = :user_id AND skill_name = :old_skill_name AND skill_level = :old_skill_level");
+        $stmt->bindParam(":new_skill_name", $ColumnsValues["new_skill_name"]);
+        $stmt->bindParam(":new_skill_level", $ColumnsValues["new_skill_level"]);
+        $stmt->bindParam(":user_id", $ColumnsValues["user_id"]);
+        $stmt->bindParam(":old_skill_name", $ColumnsValues["old_skill_name"]);
+        $stmt->bindParam(":old_skill_level", $ColumnsValues["old_skill_level"]);
+        $stmt->execute();
+    }
+
+    // delete a skill
+    public function DeleteData($ColumnsValues){
+        $stmt = $this->dbconn->prepare("DELETE FROM skills_section WHERE user_id = :user_id AND skill_name = :skill_name AND skill_level = :skill_level");
+        $stmt->bindParam(":user_id", $ColumnsValues["user_id"]);
+        $stmt->bindParam(":skill_name", $ColumnsValues["old_skill_name"]);
+        $stmt->bindParam(":skill_level", $ColumnsValues["old_skill_level"]);
+        $stmt->execute();
+    }
+}
+
+?>
