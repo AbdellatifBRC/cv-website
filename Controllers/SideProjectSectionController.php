a79d039d9a7832d1ffad2720a775bb722c95a5c4
lalouikarim <lalouikarim@gmail.com>
2022-03-05 11:52:44 +0100

    users can delete a side project

--- Controllers/SideProjectSectionController.php
+++ Controllers/SideProjectSectionController.php
@@ -53,7 +53,37 @@ class SideProjetSectionController extends CvSectionController{
 
     // delete a side project
     public function DeleteData(){
+        // this array will be sent as a response to the client
+        $response_array["action_completed"] = false;
+        $response_array["error"] = "";
         
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["old_side_project_title"])){
+            try{
+                if($this->sectionModel->auth->isLoggedIn()){
+                    // indicate that the user is logged in
+                    $response_array["logged_in"] = true;
+
+                    // sanitize the user's input
+                    $oldSideProjectTitle = new Input($_POST["old_side_project_title"]);
+                    $oldSideProjectTitle->Sanitize();
+
+                    // get the user id
+                    $userId = $this->sectionModel->auth->getUserId();
+
+                    // delete the side project
+                    $this->sectionModel->DeleteData(array("user_id" => $userId, "old_title" => $oldSideProjectTitle->value));
+
+                    // indicate that the action has completed
+                    $response_array["action_completed"] = true;
+                } else{
+                    $response_array["logged_in"] = false;
+                }
+            } catch (Exception $e) {
+                $response_array["error"] = $e->getMessage();
+            }
+        }
+        
+        echo json_encode($response_array);
     }
 
     // add another side project subsection
