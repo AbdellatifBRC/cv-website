6e03e070bccd7da5b207e2ac3963f397aa7baf8f
lalouikarim <lalouikarim@gmail.com>
2022-03-06 14:27:35 +0100

    users can save a custom section

--- Controllers/CustomSectionController.php
+++ Controllers/CustomSectionController.php
@@ -6,7 +6,49 @@ require "CvSectionController.php";
 class CustomSectionController extends CvSectionController{
     // save a custom section
     public function SaveData(){
+        // this array will be sent as a response to the client
+        $response_array["action_completed"] = false;
+        $response_array["error"] = "";
         
+        if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["custom_section_title"]) && isset($_POST["custom_section_description"])){
+            try{
+                if($this->sectionModel->auth->isLoggedIn()){
+                    // indicate that the user is logged in
+                    $response_array["logged_in"] = true;
+
+                    // sanitize the user's input
+                    $newCustomSectionTitle = new Input($_POST["custom_section_title"]);
+                    $newCustomSectionTitle->Sanitize();
+                    $newCustomSectionDescription = new Input($_POST["custom_section_description"]);
+                    $newCustomSectionDescription->Sanitize();
+
+                    // check to see if the user already saved this custom section
+                    $oldCustomSectionTitle = new Input("");
+                    if(isset($_POST["old_custom_section_title"])){
+                        $oldCustomSectionTitle->value = $_POST["old_custom_section_title"];
+                        $oldCustomSectionTitle->Sanitize();
+                    } else{
+                        $oldCustomSectionTitle->value = null;
+                    }
+
+
+                    // get the user id
+                    $userId = $this->sectionModel->auth->getUserId();
+
+                    // save the custom section
+                    $this->sectionModel->InsertData(array("user_id" => $userId, "new_title" => $newCustomSectionTitle->value, "new_description" => $newCustomSectionDescription->value, "old_title" => $oldCustomSectionTitle->value));
+
+                    // indicate that the action has completed
+                    $response_array["action_completed"] = true;
+                } else{
+                    $response_array["logged_in"] = false;
+                }
+            } catch (Exception $e) {
+                $response_array["error"] = $e->getMessage();
+            }
+        }
+
+        echo json_encode($response_array);
     }
 
     // delete a custom section
